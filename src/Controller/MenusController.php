93ceb6fdb8ed148e5506c658d4af42c9835824a0
FloAFDEV <103335500+FloAFDEV@users.noreply.github.com>
2023-03-05 15:37:20 +0100

    //////

--- src/Controller/MenusController.php
+++ src/Controller/MenusController.php
@@ -11,6 +11,9 @@ class MenusController extends AbstractController
     #[Route('/menus', name: 'app_menus')]
     public function index(): Response
     {
-        return $this->render('menus/index.html.twig', []);
+        return $this->render('menus/index.html.twig', [
+            'controller_name' => 'MenusController',
+
+        ]);
     }
 }
